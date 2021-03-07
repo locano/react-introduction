837996af67cbb9b14d214023cb6e89e1e6b07310
Ludwing <ludwing238@gmail.com>
2021-03-07 15:16:24 -0600

    adding simpleComponents

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import Game from "./components/tictactoe";
+import StartApp from "./components/simpleComponent/startApp";
 
-ReactDOM.render(<Game />, document.getElementById("root"));
+ReactDOM.render(<StartApp />, document.getElementById("root"));
