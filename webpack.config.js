38171d6f88036fc29c5b1031afd985a2b182efe3
Ludwing <ludwing238@gmail.com>
2021-03-07 00:18:58 -0600

    adding webpack and babel config files

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: {
+    index: "./src/index.js",
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: "babel-loader",
+        exclude: /(node_modules)/,
+      },
+    ],
+  },
+};
